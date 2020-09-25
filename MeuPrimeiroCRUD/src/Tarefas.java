7f607828e7ccbb6e06fb879e31cb7258ba9595f2
Marcos Vinícius <marquinhosvinis@gmail.com>
2020-09-25 02:13:48 -0300

    Adicionando Javabean para armazenar dados

---
+++ MeuPrimeiroCRUD/src/Tarefas.java
@@ -0,0 +1,53 @@
+import java.util.Calendar;
+
+public class Tarefas {
+	private Integer id;
+	private String titulo;
+	private String conteudo;
+	private String categoria;
+	private Integer prioridade;
+	private Calendar data;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+	
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+}
